f805dfb121a84547614bf7c21eec7f56287be0d4
SnoeProductions <>
2021-05-25 05:21:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const input = require('readline-sync');
+let str = "LaunchCode";
+let str1 = str.slice(0,3);
+let str4 = str.slice(3,10);
+console.log(str1);
+let str2 = str + str1;
+console.log(str4);
+console.log(str4 + str1);
+
+//a) Use string methods to remove the first three characters from the string and add them to the end.
+//Hint - define another variable to hold the new string or reassign the new string to str.
+console.log(`This will be ${str} and dont give up ${str}.`);
+
+
+//Use a template literal to print the original and modified string in a descriptive phrase.
+
+
+
+//b) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+
+
+//c) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
